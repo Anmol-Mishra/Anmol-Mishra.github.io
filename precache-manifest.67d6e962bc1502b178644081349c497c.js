6a3ab90fd37c5ab2501f9133edb6bde5c0f4001b
Anmol Mishra <anmol.mishra9197@gmail.com>
2020-04-13 10:52:14 +0530

    First Committ
    Committing the personal website for the first time.

---
+++ precache-manifest.67d6e962bc1502b178644081349c497c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "560e471e4596cfcd4024767647014be7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36f60ed9c6c1d2447cd2",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d83c55c2afa0af2ab358",
+    "url": "/static/css/main.4aa1c004.chunk.css"
+  },
+  {
+    "revision": "36f60ed9c6c1d2447cd2",
+    "url": "/static/js/2.b2b93822.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b2b93822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d83c55c2afa0af2ab358",
+    "url": "/static/js/main.d74a6dde.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  },
+  {
+    "revision": "bc1d8cd1818ee1c41a3896dd41cb83f9",
+    "url": "/static/media/Resume_AnmolMishra.bc1d8cd1.pdf"
+  },
+  {
+    "revision": "8b9e8474f669489fe6ff20f20fd1c7c0",
+    "url": "/static/media/background.8b9e8474.jpg"
+  },
+  {
+    "revision": "bffdb2e9c1b51f1a396cf97c063296b0",
+    "url": "/static/media/myPic.bffdb2e9.jpg"
+  }
+]);
